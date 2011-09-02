685c86a03374530151797d5a74caff4a25a44527
amatsu <amatsubame@hotmail.co.jp>
2011-09-03 01:15:34 +0900

    日本語テスト

--- IntroductionOfBeer/IntroductionOfBeer/IntroductionOfBeerAppDelegate.m
+++ IntroductionOfBeer/IntroductionOfBeer/IntroductionOfBeerAppDelegate.m
@@ -2,9 +2,9 @@
 //  IntroductionOfBeerAppDelegate.m
 //  IntroductionOfBeer
 //
-//  Created by 秀典 細野 on 11/09/02.
+//  Created by Amatsu on 11/09/02.
 //  Copyright 2011年 __MyCompanyName__. All rights reserved.
-//　テスト
+//　日本語テスト
 
 #import "IntroductionOfBeerAppDelegate.h"
 
